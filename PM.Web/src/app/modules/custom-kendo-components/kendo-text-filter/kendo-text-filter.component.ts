19c7f90980511ae3ef634429ba30380a2997583e
Sagar Agola <sagar.agola@tatvasoft.com>
2022-01-04 16:32:31 +0530

    added debounce on filter change

--- PM.Web/src/app/modules/custom-kendo-components/kendo-text-filter/kendo-text-filter.component.ts
+++ PM.Web/src/app/modules/custom-kendo-components/kendo-text-filter/kendo-text-filter.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { AppConsts } from 'src/app/common/app-consts';
 import { KendoColumn } from '../models/kendo-column.model';
 
 @Component({
@@ -6,20 +9,32 @@ import { KendoColumn } from '../models/kendo-column.model';
   template: '<kendo-textbox [(ngModel)]="searchText" [clearButton]="true" (valueChange)="onChange()"></kendo-textbox>',
   styleUrls: ['./kendo-text-filter.component.scss']
 })
-export class KendoTextFilterComponent {
+export class KendoTextFilterComponent implements OnInit, OnDestroy {
 
   @Input() title: string;
   @Input() column: KendoColumn;
-
   @Output() onFilterChange: EventEmitter<KendoColumn> = new EventEmitter<KendoColumn>();
+
+  private filterChangeSubscription: Subscription;
+  private filterTextChanged: Subject<void> = new Subject<void>();
 
   searchText: string = "";
 
   constructor() { }
 
+  ngOnInit(): void {
+    this.filterChangeSubscription = this.filterTextChanged.pipe(debounceTime(AppConsts.debounceTime)).subscribe(() => {
+      this.onFilterChange.emit(this.column);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.filterChangeSubscription?.unsubscribe();
+  }
+
   onChange(): void {
     this.column.search = this.searchText;
-    this.onFilterChange.emit(this.column);
+    this.filterTextChanged.next();
   }
 
 }
